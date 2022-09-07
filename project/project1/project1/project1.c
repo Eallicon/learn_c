1e68fe97fa45cd8aa38ecf726b2b5e8efd65ad77
lyz <1282602701@qq.com>
2022-09-07 18:34:37 +0800

    提交

---
+++ project/project1/project1/project1.c
@@ -0,0 +1,19 @@
+﻿// # 指令形式  include 是包含  stdio.h这个文件 
+//  stdio.h  标准输入输出的文件
+#include <stdio.h>
+
+
+char main() { // 有且只有一个
+	
+	
+
+	// printf("%d\n", sizeof(char));  // 1
+	// printf("%d\n", sizeof(int));   // 4
+	// printf("%d\n", sizeof(short)); // 2
+	// printf("%d\n", sizeof(long));  // 8 或者 4
+	// printf("%d\n", sizeof(long long));  // 8 
+	// printf("%d\n", sizeof(float)); // 4
+	// printf("%d\n", sizeof(double)); // 8
+
+	return "123";
+}
