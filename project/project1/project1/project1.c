96b6cd81425f52815f117f1a2d952f00ca67eb5e
yzJ <1282602701@qq.com>
2022-09-07 21:02:43 +0800

    家

--- project/project1/project1/project1.c
+++ project/project1/project1/project1.c
@@ -1,12 +1,28 @@
-﻿// # 指令形式  include 是包含  stdio.h这个文件 
+﻿ #define _CRT_SECURE_NO_WARNINGS
+// # 指令形式  include 是包含  stdio.h这个文件 
 //  stdio.h  标准输入输出的文件
 #include <stdio.h>
 
+int mun2 = 20;
 
-char main() { // 有且只有一个
-	
-	
+int main() { // 有且只有一个
+	// 在C语言中 变量需要在代码块顶部输出 
+	int mun1 = 0;
+	int num2 = 0;
+	scanf("%d%d", &mun1,&num2);  // 需要使用取地址符  & 取地址符  也就是需要把 输入的值 放到内存中 的屋子里面去
+	int sum = num2 + mun1;
+	printf("mun1 = %d \n", mun1);
+	printf("num2 = %d \n", num2);
+	printf("sum = %d \n", sum);
 
+
+
+
+
+
+	// int num1 = 10; // 局部变量
+	// int num2 = 30; 
+	// printf("%d \n", num2);
 	// printf("%d\n", sizeof(char));  // 1
 	// printf("%d\n", sizeof(int));   // 4
 	// printf("%d\n", sizeof(short)); // 2
@@ -15,5 +31,5 @@ char main() { // 有且只有一个
 	// printf("%d\n", sizeof(float)); // 4
 	// printf("%d\n", sizeof(double)); // 8
 
-	return "123";
+	return 0;
 }
